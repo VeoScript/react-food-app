d1adb49ea1b8f6f6f4b1db56090e31354a8a79db
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-05-12 11:17:43 +0800

    chore: update image not displaying in github page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/react-food-app'
 }
 
 module.exports = nextConfig
--- src/mocks/data.ts
+++ src/mocks/data.ts
@@ -1,27 +1,27 @@
 export const images = [
   {
     id: 1,
-    src: "/images/adobo.jpg",
+    src: `${process.env.NODE_ENV === 'production' ? '/react-food-app/images/adobo.jpg' : '/images/adobo.jpg'}`,
     caption: "Filipino Adobo",
   },
   {
     id: 2,
-    src: "/images/caldereta.jpg",
+    src: `${process.env.NODE_ENV === 'production' ? '/react-food-app/images/caldereta.jpg' : '/images/caldereta.jpg'}`,
     caption: "My specialty Caldereta",
   },
   {
     id: 3,
-    src: "/images/scrambled-egg.jpg",
+    src: `${process.env.NODE_ENV === 'production' ? '/react-food-app/images/scrambled-egg.jpg' : '/images/scrambled-egg.jpg'}`,
     caption: "Scrambled Egg",
   },
   {
     id: 4,
-    src: "/images/afritada.jpg",
+    src: `${process.env.NODE_ENV === 'production' ? '/react-food-app/images/afritada.jpg' : '/images/afritada.jpg'}`,
     caption: "Afritada na Masarap",
   },
   {
     id: 5,
-    src: "/images/kinilaw.jpg",
+    src: `${process.env.NODE_ENV === 'production' ? '/react-food-app/images/kinilaw.jpg' : '/images/kinilaw.jpg'}`,
     caption: "Kinilaw na Isda",
   },
 ];
